41f19bbc90e4f6fad91060fb768c5986721895c9
Isrealsix's MacBook Pro <isrealsix@gmail.com>
2023-10-21 17:55:40 +0700

    set default settings to renderer

---
+++ app/js/renderer.js
@@ -0,0 +1,5 @@
+const { ipcRenderer } = require('electron');
+ipcRenderer.on('settings:get', (e, settings) => {
+  document.getElementById('cpu-overload').value = settings.cpuOverload
+  document.getElementById('alert-frequency').value = settings.alertFrequency
+})
