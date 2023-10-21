289cb4ec0f48057a2af5380060413264a7f1eed6
Isrealsix's MacBook Pro <isrealsix@gmail.com>
2023-10-21 18:38:59 +0700

    saved data to local file

--- app/js/renderer.js
+++ app/js/renderer.js
@@ -1,5 +1,39 @@
 const { ipcRenderer } = require('electron');
+const settingsForm = document.getElementById('settings-form');
+
+// Get settings
 ipcRenderer.on('settings:get', (e, settings) => {
-  document.getElementById('cpu-overload').value = settings.cpuOverload
-  document.getElementById('alert-frequency').value = settings.alertFrequency
+  const cpuOverload = document.getElementById('cpu-overload')
+  const alertFrequency = document.getElementById('alert-frequency')
+
+  cpuOverload.value = settings.cpuOverloadValue
+  alertFrequency.value = settings.alertFrequencyValue
 })
+
+// Submit settings
+settingsForm.addEventListener('submit', ev => {
+  ev.preventDefault()
+  const cpuOverload = document.getElementById('cpu-overload')
+  const alertFrequency = document.getElementById('alert-frequency')
+  const cpuOverloadValue = cpuOverload.value
+  const alertFrequencyValue = alertFrequency.value
+
+  // Set new settings to main process
+  ipcRenderer.send('settings:set', {
+    cpuOverloadValue,
+    alertFrequencyValue
+  })
+
+  showAlert('settings saved');
+})
+
+function showAlert(msg) {
+  const alert = document.getElementById('alert')
+  alert.classList.remove('hide')
+  alert.classList.add('alert')
+  alert.innerText = msg
+
+  setTimeout(() => {
+    alert.classList.add('hide')
+  }, 3000);
+}
